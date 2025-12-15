beb6a0ee49d8a91865262f8ca1ff21950d591d3f
Pablo Rian <pablo@sendspark.com>
2025-12-14 23:05:13 -0300

    test deployment 9

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '../../'),
+  output: 'standalone',
+  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   serverExternalPackages: ['mongoose', 'mongodb', '@repo/database'],
 
   typescript: {
