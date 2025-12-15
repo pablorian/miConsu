af807a28c7b3e32e73b017adfa2c7b98fb4c908d
Pablo Rian <pablo@sendspark.com>
2025-12-14 21:31:13 -0300

    Fixing lint

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['mongoose', 'mongodb', '@repo/database'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   }
